ae5e684c1427c700f0f922dc7e014e0c458d727f
Cu-Hoang <cungochoang2003@gmail.com>
2024-11-18 23:59:00 +0700

    initial project

---
+++ src/main/java/com/project/ecommercebase/EcommercebaseApplication.java
@@ -0,0 +1,13 @@
+package com.project.ecommercebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ecommercebase/EcommercebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ecommercebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
